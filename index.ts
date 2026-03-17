fbe3ded804a7fcb66053cd46325f5c7978070b0f
Szymon Jakub Wypler <wyplerszymon0@gmail.com>
2026-03-17 13:56:51 +0100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { inferSchema, inferSchemaFromString } from './infer';
+export type { InferOptions } from './infer';
+export { generateFromDescription } from './ai-gen';
+export type { AiGenOptions } from './ai-gen';
